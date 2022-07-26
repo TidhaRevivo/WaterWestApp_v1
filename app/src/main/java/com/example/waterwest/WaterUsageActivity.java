a27b0664bdf86a3c18b5aaf8b0117d7d99096858
Omriz32 <93472411+Omriz32@users.noreply.github.com>
2022-07-26 22:08:12 +0300

    buttons added

--- app/src/main/java/com/example/waterwest/WaterUsageActivity.java
+++ app/src/main/java/com/example/waterwest/WaterUsageActivity.java
@@ -1,37 +1,36 @@
 package com.example.waterwest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    Button LitersBtn, AlertBtn;
+public class WaterUsageActivity extends AppCompatActivity {
+Button HomeBtn, AlertBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_water_usage);
+        HomeBtn = findViewById(R.id.HomeBtn);
+        HomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(WaterUsageActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         AlertBtn = findViewById(R.id.AlertBtn);
         AlertBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(MainActivity.this,AlertActivity.class);
+                Intent intent = new Intent(WaterUsageActivity.this,AlertActivity.class);
                 startActivity(intent);
             }
         });
 
-        LitersBtn = findViewById(R.id.LitersBtn);
-        LitersBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(MainActivity.this,WaterUsageActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
