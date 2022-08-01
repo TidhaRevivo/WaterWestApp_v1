9262a9cf69d5c4e47c2637458a99a5f8d3d5e7b8
TidhaRevivo <76057684+TidhaRevivo@users.noreply.github.com>
2022-08-01 22:28:23 +0300

    Update RPiDummyData.py

--- RPiDummyData.py
+++ RPiDummyData.py
@@ -2,14 +2,10 @@ import RPi.GPIO as GPIO
 import time
 import math
 from pyrebase import pyrebase
+import json
 
-config = {
-  "apiKey": "GrlM80waITypgrHp2K2mWCO0Td6zDI2TFBprdh8h",
-  "authDomain": "dummy-project-4b51c.firebaseapp.com",
-  "databaseURL": "https://dummy-project-4b51c-default-rtdb.firebaseio.com/",
-  "storageBucket": "dummy-project-4b51c.appspot.com"
-}
 
+# Get RPi's CPU ID _______________________________________
 def getserial():
   # Extract serial from cpuinfo file
   cpuserial = "0000000000000000"
@@ -24,15 +20,30 @@ def getserial():
  
   return cpuserial
 
+# __________________________________________
+
+
+# Connetcting to DB _____________________________________________-
+config = {
+  "apiKey": "GrlM80waITypgrHp2K2mWCO0Td6zDI2TFBprdh8h",
+  "authDomain": "dummy-project-4b51c.firebaseapp.com",
+  "databaseURL": "https://dummy-project-4b51c-default-rtdb.firebaseio.com/",
+  "storageBucket": "dummy-project-4b51c.appspot.com"
+}
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-CpuSerial= getserial()
-GPIO.setmode(GPIO.BOARD)
+# _______________________________________________________________
+
+CpuSerial= getserial() #CPU Serial number- basically RPi ID
+GPIO.setmode(GPIO.BOARD) #Set mode to work with pin numbers and not ...
 GPIO.setup(18,GPIO.OUT) #trigger
 GPIO.setup(16,GPIO.IN) #echo
 Radius = 0.129 #meters
 Height = 0.38 #meters
-Full_Tank = 20
+Full_Tank = 20 #leters
+
+# my_time = time.strftime('%Y-%m-%d', time.localtime(time.time())) #code to trasnfer time.time() to Date
+
 # Full_Tank = math.pi * Radius ** 2 * Height * 1000
 
 
@@ -55,20 +66,18 @@ while True:
 		while GPIO.input(16)==1:
 			pulse_end = time.time()
 
-		#calculate distance in centemeteres
 		pulse_duration = pulse_end - pulse_start
 		distance = pulse_duration * 171.5
 		distance = round(distance, 2) #round it to two decimal place
 		Empty_Tank = math.pi * Radius ** 2 * distance * 1000
 		Water = round((Full_Tank - Empty_Tank), 3)
 		data = {
-			"Water Level" : Water,
-			"Date" : time.time()
+			str(time.strftime('%Y-%m-%d', time.localtime(time.time()))): Water
 		}
 		
-		print (str(Full_Tank) + " - " + str(Empty_Tank) + " = " + str(Water))
-		print(distance, CpuSerial)
-		db.child("Water_Level_Db").child(CpuSerial).push(data)
+		# print (str(Full_Tank) + " - " + str(Empty_Tank) + " = " + str(Water))
+		print(str(distance * 100) + "cm", str(time.time()).split('.')[0])
+		db.child("RaspberryPis").child(CpuSerial).child(str(time.time()).split('.')[0]).push(data)
 	#cleanup on ctr c
 	except KeyboardInterrupt:
 		GPIO.cleanup()
